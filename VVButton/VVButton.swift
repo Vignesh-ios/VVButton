aa6e406f11ab6c9ffd4e8a1a6d6eae579291b236
Vignesh <vignesh0177@gmail.com>
2018-09-19 10:25:02 +0530

    first commit

---
+++ VVButton/VVButton.swift
@@ -0,0 +1,21 @@
+//
+//  VVButton.swift
+//  VVButton
+//
+//  Created by Vignesh VV on 19/09/18.
+//  Copyright © 2018 Vignesh VV. All rights reserved.
+//
+
+import UIKit
+
+class VVButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
